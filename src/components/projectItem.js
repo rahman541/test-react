f012bea4074f2da91c870192c2d32c64bfaf8d2e
Rahman <rahmanromli@yahoo.com>
2017-09-13 05:18:10 +0800

    Display item component

--- src/components/projectItem.js
+++ src/components/projectItem.js
@@ -4,7 +4,7 @@ class ProjectItem extends Component {
 	render() {
 		return (
 			<li className="ProjectItem">
-				
+				<strong>{this.props.project.title} - {this.props.project.category}</strong>
 			</li>
 		);
 	}
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import ProjectItem from './projectItem';
 
 class Projects extends Component {
 	render() {
 		let projectItems;
 		if (this.props.projects) {
 			projectItems = this.props.projects.map(project => {
-				console.log(project);
-			})
+				// console.log(project);
+				return (
+					<ProjectItem key={project.title} project={project} />
+				);
+			});
 		}
 		return (
 			<div className="Projects">
-				My Projects
+				{projectItems}
 			</div>
 		);
 	}
